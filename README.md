017dcac64458bb4395d7f70868f3309461687f51
sergaben <gabense@hotmail.com>
2018-09-25 21:32:35 +0100

    expecting to commit more often to these repo

---
+++ README.md
@@ -0,0 +1,3 @@
+## A collection of simple example to help me learn and deepen my understanding of the Akka toolkit
+
+Some of the examples are going to be taken and tweaked from the main Akka documentation website
---
+++ src/main/scala/sample/hello/FailureHandlingActors.scala
@@ -0,0 +1,30 @@
+package sample.hello
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+class SupervisingActor extends Actor {
+  val child: ActorRef = context.actorOf(Props[SupervisedActor], "supervised-actor")
+
+  override def receive: Receive = {
+    case "failChild" ⇒ child ! "fail"
+  }
+}
+
+class SupervisedActor extends Actor {
+  override def preStart(): Unit = println("supervised actor started")
+  override def postStop(): Unit = println("supervised actor stopped")
+
+  override def receive: Receive = {
+    case "fail" ⇒
+      println("supervised actor fails now")
+      throw new Exception("I failed!")
+  }
+}
+
+object FailureHandlingActors extends App{
+  val system = ActorSystem("testSystem")
+
+  val supervisingActor = system.actorOf(Props[SupervisingActor],"supervising-actor")
+
+  supervisingActor ! "failChild"
+
+}
